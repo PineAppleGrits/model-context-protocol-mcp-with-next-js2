b202af7be08dbac9ebb96cd066450e3cd2f7009f
Gino S <ginossomi@gmail.com>
2025-07-29 14:10:02 -0300

    add new tools

--- app/[transport]/route.ts
+++ app/[transport]/route.ts
@@ -13,12 +13,24 @@ const handler = createMcpHandler(
         content: [{ type: "text", text: `Tool echo: ${message}` }],
       })
     );
+    server.tool("get-transport", "description", {}, async () => ({
+      content: [{ type: "text", text: `Tool get-transport` }],
+    }));
+    server.tool("get_time", "description", {}, async () => ({
+      content: [{ type: "text", text: new Date().toISOString() }],
+    }));
   },
   {
     capabilities: {
       tools: {
         echo: {
           description: "Echo a message",
+        },
+        getTransport: {
+          description: "Get transport",
+        },
+        getTime: {
+          description: "Get time",
         },
       },
     },
